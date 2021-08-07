4940b6b4be1deb64af626b7e736fbf8f4c92d565
Jeffrey Martin <unicycle77@gmail.com>
2021-08-06 17:50:05 -0700

    Style Dog List and Dog Details

--- src/DogList.js
+++ src/DogList.js
@@ -6,13 +6,15 @@ export default class DogList extends Component {
   render() {
     return (
       <div className="DogList">
-        <h2 className="display-1 text-center">Dog List!</h2>
+        <h2 className="display-1 text-center mt-3 mb-5">Dog List!</h2>
         <div className="row">
           {this.props.dogs.map((dog) => (
             <div className="Dog col-md-6 col-lg-4 text-center" key={dog.name}>
               <Link to={`/dogs/${dog.name}`}>
                 <img src={dog.src} alt={dog.name} />
-                <h2>{dog.name}</h2>
+              </Link>
+              <Link className="underline" to={`/dogs/${dog.name}`}>
+                <h2 className="mt-3">{dog.name}</h2>
               </Link>
             </div>
           ))}
