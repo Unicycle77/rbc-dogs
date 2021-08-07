d5e0eacc6610966671f197a878132ac0b6205b68
Jeffrey Martin <unicycle77@gmail.com>
2021-08-06 17:12:48 -0700

    Refactor routes and style dog list

--- src/DogList.js
+++ src/DogList.js
@@ -7,17 +7,15 @@ export default class DogList extends Component {
     return (
       <div className="DogList">
         <h2 className="display-1 text-center">Dog List!</h2>
-        <div className="container">
-          <div className="row">
-            {this.props.dogs.map((dog) => (
-              <div className="Dog col-md-6 col-lg-4 text-center" key={dog.name}>
-                <Link to={`/dogs/${dog.name}`}>
-                  <img src={dog.src} alt={dog.name} />
-                  <h2>{dog.name}</h2>
-                </Link>
-              </div>
-            ))}
-          </div>
+        <div className="row">
+          {this.props.dogs.map((dog) => (
+            <div className="Dog col-md-6 col-lg-4 text-center" key={dog.name}>
+              <Link to={`/dogs/${dog.name}`}>
+                <img src={dog.src} alt={dog.name} />
+                <h2>{dog.name}</h2>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     );
